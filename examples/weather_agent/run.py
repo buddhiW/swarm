288f154b452adf300b7c25cf5067c58a1e428227
Buddhi Wickramasinghe <bcwicki@gmail.com>
2024-10-31 16:02:40 -0700

    Add env keys

--- examples/weather_agent/run.py
+++ examples/weather_agent/run.py
@@ -1,5 +1,16 @@
+import sys
+import os
+from dotenv import load_dotenv
+
+# setting path
+sys.path.append('..\swarm')
+
 from swarm.repl import run_demo_loop
 from agents import weather_agent
+
+## Setup the API keys
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 if __name__ == "__main__":
     run_demo_loop(weather_agent, stream=True)
